6a20612cf192e325ff4b62bd2d07fd1eea684eee
kc71486 <linda20030102@gmail.com>
2022-04-29 10:19:40 +0800

    up to 4.2

--- app.js
+++ app.js
@@ -31,11 +31,21 @@ function create() {
 	// Step 1.2 code goes here
 	this.add.image(400, 300, 'sky')
 	// Step 2 code goes here
-
+	platforms = this.physics.add.staticGroup()
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+	platforms.create(600, 400, 'ground').setScale(1, .5).refreshBody()
+	platforms.create( 50, 250, 'ground').setScale(1, .5).refreshBody()
+	platforms.create(750, 220, 'ground').setScale(1, .5).refreshBody()
 	// Step 3 code goes here
-
+	player = this.physics.add.sprite(100, 450, 'dude')
+	player.setBounce(.2)
+	player.setCollideWorldBounds(true)
 	// Step 4.1 code goes here
+	this.anims.create({ frameRate: 10, frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }), key: 'left', repeat: -1 })
+	this.anims.create({ frameRate: 20, frames: [ { key: 'dude', frame: 4 } ], key: 'turn', })
+	this.anims.create({ frameRate: 10, frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }), key: 'right', repeat: -1 })
 
+	cursors = this.input.keyboard.createCursorKeys()
 	// Step 4.3 code goes here
 
 	// Step 5.1 code goes here
@@ -49,6 +59,19 @@ function create() {
 
 function update() {
 	// Step 4.2 code goes here
+	const speed = 160
+	if (cursors.left.isDown) {
+		player.setVelocityX(-speed)
+		player.anims.play('left', true)
+	} else if (cursors.right.isDown) {
+		player.setVelocityX(speed)
+		player.anims.play('right', true)
+	} else {
+		player.setVelocityX(0)
+		player.anims.play('turn')
+	}
+	if (cursors.up.isDown && player.body.touching.down)
+		player.setVelocityY(-330)
 }
 
 function collectStar(player, star) {
